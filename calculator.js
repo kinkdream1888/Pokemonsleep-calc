e32cc3b9b1abc8d693ea659bf55fa46777d3b0dd
kinkdream1888 <kinkdream1888@gmail.com>
2026-07-12 15:25:31 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -166,10 +166,11 @@ function helperOverflowAnalysis(selectedSubs, lines) {
 }
 
 function calculate() {
+    let useRealistic = window.useRealistic || false;
     let calcType = typeSelect.value;
     let nature = natureSelect.value;
     let selectedSubs = getSelectedSubs();
-    let useRealistic = realisticCheckbox.checked;
+    let useRealistic = useRealistic.checked;
 
     if (calcType === '食材型') {
         let pokeValue = pokeSelect.value;
